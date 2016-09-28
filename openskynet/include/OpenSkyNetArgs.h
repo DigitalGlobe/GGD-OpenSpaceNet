a043409a3f5636a57e55afc89adda1ccbdea5d9c
kevinmcgee <kevin.mcgee@digitalglobe.com>
2016-09-28 12:32:33 -0400

    This file now only contains the arguments for OSN

--- openskynet/include/OpenSkyNetArgs.h
+++ openskynet/include/OpenSkyNetArgs.h
@@ -24,8 +24,6 @@
 #ifndef OPENSKYNET_OPENSKYNETARGS_H
 #define OPENSKYNET_OPENSKYNETARGS_H
 
-#include <boost/program_options.hpp>
-#include <utility/Logging.h>
 #include <vector/Feature.h>
 
 #define OSN_LOG(sev) DG_LOG(OpenSkyNet, sev)
@@ -94,49 +92,6 @@ public:
 
     // Logging options
     bool quiet = false;
-    dg::deepcore::level_t consoleLogLevel = dg::deepcore::level_t::info;
-    std::string fileLogPath;
-    dg::deepcore::level_t fileLogLevel = dg::deepcore::level_t::debug;
-
-    OpenSkyNetArgs();
-    void parseArgsAndProcess(int argc, const char* const* argv);
-
-private:
-    bool confidenceSet = false;
-    bool mapIdSet = false;
-    bool displayHelp = false;
-
-    void setupInitialLogging();
-    void setupLogging();
-    void parseArgs(int argc, const char* const* argv);
-    void maybeDisplayHelp(boost::program_options::variables_map vm);
-    void printUsage(Action action=Action::UNKNOWN) const;
-    void readArgs(boost::program_options::variables_map vm, bool splitArgs=false);
-    void readWebServiceArgs(boost::program_options::variables_map vm, bool splitArgs=false);
-    void promptForPassword();
-    void readOutputArgs(boost::program_options::variables_map vm, bool splitArgs=false);
-    void readProcessingArgs(boost::program_options::variables_map vm, bool splitArgs=false);
-    void readFeatureDetectionArgs(boost::program_options::variables_map vm, bool splitArgs=false);
-    void readLoggingArgs(boost::program_options::variables_map vm, bool splitArgs=false);
-
-    void validateArgs();
-
-    boost::program_options::options_description localOptions_;
-    boost::program_options::options_description webOptions_;
-    boost::program_options::options_description outputOptions_;
-    boost::program_options::options_description processingOptions_;
-    boost::program_options::options_description detectOptions_;
-    boost::program_options::options_description loggingOptions_;
-    boost::program_options::options_description generalOptions_;
-
-    boost::program_options::options_description allOptions_;
-    boost::program_options::options_description visibleOptions_;
-    boost::program_options::options_description optionsDescription_;
-
-    std::vector<std::string> supportedFormats_;
-
-    boost::shared_ptr<deepcore::log::sinks::sink> cerrSink_;
-    boost::shared_ptr<deepcore::log::sinks::sink> coutSink_;
 };
 
 } } // namespace dg { namespace osn {
