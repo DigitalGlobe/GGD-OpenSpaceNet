3a463e26d4a5359e2f6bc55455dd701139cfefeb
Aleksey Vitebskiy <aleksey.vitebskiy@digitalglobe.com>
2017-04-28 21:34:57 -0400

    Adjusted after moving NMS to geometry.

--- src/OpenSpaceNet.cpp
+++ src/OpenSpaceNet.cpp
@@ -27,11 +27,11 @@
 #include <boost/property_tree/json_parser.hpp>
 #include <boost/range/algorithm/copy.hpp>
 #include <classification/GbdxModelReader.h>
-#include <classification/NonMaxSuppression.h>
 #include <future>
 #include <geometry/AffineTransformation.h>
 #include <geometry/CvToLog.h>
 #include <geometry/FilterLabels.h>
+#include <geometry/NonMaxSuppression.h>
 #include <geometry/PassthroughRegionFilter.h>
 #include <geometry/TransformationChain.h>
 #include <imagery/GdalImage.h>
